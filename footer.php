80e0b224360174120fabb3ac0b586483f5d59e07
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-05-23 19:10:55 +0530

    commit updated

--- footer.php
+++ footer.php
@@ -7,6 +7,6 @@
                         <a>Blog</a>
                         <a>Contact</a>
                     </div>
-                    <div class="copyright t-grey center">2019 - All rights reserved to &copy;Exodus</div>
+                    <div class="copyright t-grey center">2019 - All rights reserved to &copy;TravelGuru Read <a href="terms-and-conditions.php">Terms-and-Conditions</a> and <a href="privacy-policy.php">Privacy-Policy</a></div>
                 </div>
             </footer>
