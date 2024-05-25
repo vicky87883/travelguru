1a8bf2cd78842a12839273fbe17512a2023e4a69
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-05-25 20:43:56 +0530

    commit updated

--- ladakh.php
+++ ladakh.php
@@ -1,148 +1,104 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <title>Ladakh region, Kashmir region, Indian subcontinent, Asia</title>
-        <meta name="description" content="Ladakh
+<html xmlns=http://www.w3.org/1999/xhtml lang=en-US>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<title>Ladakh region, Kashmir region, Indian subcontinent, Asia</title>
+<meta name=description content="Ladakh
 region, Kashmir region, Indian subcontinent, Asia">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="keywords" content="Ladakh
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<meta name=keywords content="Ladakh
 region, Kashmir region, Indian subcontinent, Asia">
-        <meta name="Resource-type" content="Document">
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
-        
-        <!-- Upload Font "https://fonts.google.com/" -->
-        <link href="../../css?family=Montserrat:100,200,300,400,600,700&display=swap" rel="stylesheet">
-        <link href="../../css-1?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel="stylesheet">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-        <!-- Styles CSS -->
-        <link rel="stylesheet" type="text/css" href="css/style.css">
-        <link rel="stylesheet" type="text/css" href="css/az-styles.css">
-        <!-- Load Font Awesome Kit -->
-        <script src="../../747940bb1c.js"></script>
-        <!-- Load Animate CSS -->
-        <link rel="stylesheet" type="text/css" href="css/animate.css">
-        <!-- Load AOS CSS -->
-        <link rel="stylesheet" type="text/css" href="css/aos.css">
-        <!-- Slick Slider CSS -->
-        <link rel="stylesheet" type="text/css" href="css/slick.css">
-        <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755"
-     crossorigin="anonymous"></script>
-     
-    </head>
-    <body class="white">
-        
-        <!-- Main Content Starts -->
-        <?php include('header.php') ?>
-        
-        <!-- Section -->
-        <div id="about" class="section video"> 
-                <div class="container"> 
-                    <div class="content center mt-100">
-                        <h5 class="t-gold">LADAKH</h5>
-                        <span class="line"></span>
-                        <div class="tags"><a class="tag t-white b-gold">#traveling</a> <a class="tag t-white b-gold">#India</a> <a class="tag t-white b-gold">#vacation</a></div>
-                        <div class="mt-50"></div>
-                    </div>
-                </div>
-            <!-- Video Background -->
-            <div class="video-back">
-                <div class="color-overlay op-6"></div>
-                <video class="back  img ladakh">
-                </video>
-            </div>
-        </div><!-- Section Ends -->
-        
-        <!-- Section -->
-        <div class="section white no-padding post-detail"> 
-            <div class="container" id="post"> 
-                <div class="content center paragraph">
-                    <div class="center info-box-detail mt-minus-200">
-                        <div class="author"><img src="assets/img/user1.jpg" alt="exodus" class="img-fluid"></div>
-                        <div class="post-info">By: <a class="author t-gold">Vikram</a> from <span class="country">India</span></div>
-                    </div>
-                    <h1 class="t-black">Ladakh
+<meta name=Resource-type content=Document>
+<link rel=icon type=image/x-icon href=assets/img/favicon.ico>
+<link href="../../css?family=Montserrat:100,200,300,400,600,700&display=swap" rel=stylesheet>
+<link href="../../css-1?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel=stylesheet>
+<link rel=stylesheet type=text/css href=css/bootstrap.min.css>
+<link rel=stylesheet type=text/css href=css/style.css>
+<link rel=stylesheet type=text/css href=css/az-styles.css>
+<script src=../../747940bb1c.js></script>
+<link rel=stylesheet type=text/css href=css/animate.css>
+<link rel=stylesheet type=text/css href=css/aos.css>
+<link rel=stylesheet type=text/css href=css/slick.css>
+<link rel=stylesheet type=text/css href=css/slick-theme.css>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+</head>
+<body class=white>
+<?php include('header.php') ?>
+<div id=about class="section video">
+<div class=container>
+<div class="content center mt-100">
+<h5 class=t-gold>LADAKH</h5>
+<span class=line></span>
+<div class=tags><a class="tag t-white b-gold">#traveling</a> <a class="tag t-white b-gold">#India</a> <a class="tag t-white b-gold">#vacation</a></div>
+<div class=mt-50></div>
+</div>
+</div>
+<div class=video-back>
+<div class="color-overlay op-6"></div>
+<video class="back img ladakh">
+</video>
+</div>
+</div>
+<div class="section white no-padding post-detail">
+<div class=container id=post>
+<div class="content center paragraph">
+<div class="center info-box-detail mt-minus-200">
+<div class=author><img src=assets/img/user1.jpg alt=exodus class=img-fluid></div>
+<div class=post-info>By: <a class="author t-gold">Vikram</a> from <span class=country>India</span></div>
+</div>
+<h1 class=t-black>Ladakh
 region, Kashmir region, Indian subcontinent, Asia</h1>
-                    
-                     <div class="info-box-detail left">
-                        <div class="post-date">6:25 pm Saturday, 25 May 2024</div>
-                        <span class="separation">/</span>
-                        <div class="post-views">
-                            <img src="assets/img/user1.jpg" alt="exodus" class="img-fluid">
-                            <img src="assets/img/user2.jpg" alt="exodus" class="img-fluid">
-                            <img src="assets/img/user3.jpg" alt="exodus" class="img-fluid">
-                            <span class="count">+34</span>
-                        </div>
-                    </div>
-                    
-                    <p class=" justify">Ladakh is a sizable portion of the northwest Indian subcontinent's Kashmir region, both east and north. Administratively, Ladakh is split between China, which also controls parts of northeastern Ladakh, and Pakistan (northwest), which is part of Gilgit-Baltistan, and India (southeast), which is part of Ladakh union territory (until October 31, 2019, part of Jammu & Kashmir state).</p>
-
-                    
-
-                    <p class="justify">The upper Indus River valley and the Ladakh Range, a southeastern extension of the Karakoram Range, are located in Ladakh, which has an approximate land area of 45,000 square miles (117,000 square km). One of the world's highest places is Ladakh. Deep valleys and elevated plains make up the majority of its natural characteristics. The east is dominated by the high plain, which gradually gets smaller toward the west. Rupshu is a region of big, brackish lakes in southeast Ladakh that is uniformly elevated at roughly 13,500 feet (4,100 meters). </p> 
-                    <p class="justify">The Zaskar Range is located northwest of Rupshu. It is an inhospitable area where people and cattle spend most of the year indoors due to the cold. The Zaskar River drains Zaskar and flows north to join the Indus River below Leh. In the northern part of Ladakh, farmers in valley settlements at an elevation of between 2,750 and 4,550 meters (9,000 to 15,000 feet) engage in irrigation and manuring agriculture. In the highland valleys that are too high for farming, shepherds look after their flocks. The most accessible town in Ladakh, Leh, is situated 160 miles (260 km) east of Srinagar and is a major trading hub.</p>
-                    <br>
-                    <div class="container">
-                    <img src="assets/img/backs/bikes.webp"  width="100%" height="100%"/>
-                    </div>
-                    <br>
-                    <p class="justify">Ladakh has a dry, chilly climate. Roughly 3 inches (80 mm) of precipitation falls on average each year; fine, dry, flaking snow is common and can occasionally fall in large amounts. The only places where vegetation grows are valleys and protected areas. Here, tamarisk (genus Tamarix) shrubs, furze (also known as gorse; spiky plants of the legume family), and other plants grow slowly and provide much-needed firewood. Wheat, barley, millet, buckwheat, peas, beans, and turnips are the main produce. The main products are textiles, namely woolen cloth.</p>
-                    <p class="justify">Since the breakup of British India in 1947, India and Pakistan have been at odds over Ladakh; with the 1949 cease-fire agreement, the southeast of the region was given to India and the remaining half to Pakistan. When Chinese forces arrived in the area in the early 1960s, they took control of the part of Ladakh that was theirs.</p>
-                    <blockquote><i class="fas fa-quote-right"></i> <span class="quote">I actually found the place by luck, I didn’t ask anyone for directions, I just drove my car up and down the Ladakh subcontinent <span class="author"> Mr. Ransom</span></span></blockquote>
-                </div>
-            </div>
-
-            
-            <div class="container mt-20 paragraph"> 
-                <div class="content center">
-                    <p class="mt-50"><button href="" class="primary-btn white b-gold border t-gold" onclick="like()"><i class="fas fa-heart"></i>
-    Likes: <span id="likeCount">0</span>
-</button> <button href="" class="primary-btn white b-gold border t-gold"><i class="fas fa-share"></i> Share</button></p>
-                </div>
-            </div>
-        </div>
-        <!-- Section -->
-        
-        <!-- Section -->
-        <div class="section back five mt-50">
-            <div class="container center">
-                <h5 class="t-black">stay connected</h5>
-                <span class="line gold"></span>
-
-                <div class="social-container m-20 center">
-                    <a href="" class="primary-btn white icon t-gold"><i class="fab fa-facebook-square"></i></a>
-                    <a href="" class="primary-btn white icon t-gold"><i class="fab fa-instagram"></i></a>
-                    <a href="" class="primary-btn white icon t-gold"><i class="fab fa-linkedin"></i></a>
-                </div>
-            </div>
-        </div><!-- Section Ends -->
-        
-            <!-- Footer -->
-            <?php include('footer.php') ?>
-        
-        <!-- JS files upload -->
-        <script src="js/jquery-3.4.1.min.js"></script> 
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/slick.min.js"></script>
-        <script src="js/validator.min.js"></script>
-        <script src="js/contact.js"></script> 
-        <script src="js/main.js"></script>
-        <script src="js/aos.js"></script>
-        <script>
-        // Initialize likeCounter from localStorage if available
-        let likeCounter = localStorage.getItem("likeCounter") ? parseInt(localStorage.getItem("likeCounter")) : 0;
-        document.addEventListener("DOMContentLoaded", function () {
-            document.getElementById("likeCount").innerText = likeCounter;
-        });
-
-        function like() {
-            likeCounter++;
-            localStorage.setItem("likeCounter", likeCounter); // Save likeCounter to localStorage
-            document.getElementById("likeCount").innerText = likeCounter;
-        }
-    </script>
-    </body>
+<div class="info-box-detail left">
+<div class=post-date>6:25 pm Saturday, 25 May 2024</div>
+<span class=separation>/</span>
+<div class=post-views>
+<img src=assets/img/user1.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user2.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user3.jpg alt=exodus class=img-fluid>
+<span class=count>+34</span>
+</div>
+</div>
+<p class=justify>Ladakh is a sizable portion of the northwest Indian subcontinent's Kashmir region, both east and north. Administratively, Ladakh is split between China, which also controls parts of northeastern Ladakh, and Pakistan (northwest), which is part of Gilgit-Baltistan, and India (southeast), which is part of Ladakh union territory (until October 31, 2019, part of Jammu & Kashmir state).</p>
+<p class=justify>The upper Indus River valley and the Ladakh Range, a southeastern extension of the Karakoram Range, are located in Ladakh, which has an approximate land area of 45,000 square miles (117,000 square km). One of the world's highest places is Ladakh. Deep valleys and elevated plains make up the majority of its natural characteristics. The east is dominated by the high plain, which gradually gets smaller toward the west. Rupshu is a region of big, brackish lakes in southeast Ladakh that is uniformly elevated at roughly 13,500 feet (4,100 meters). </p>
+<p class=justify>The Zaskar Range is located northwest of Rupshu. It is an inhospitable area where people and cattle spend most of the year indoors due to the cold. The Zaskar River drains Zaskar and flows north to join the Indus River below Leh. In the northern part of Ladakh, farmers in valley settlements at an elevation of between 2,750 and 4,550 meters (9,000 to 15,000 feet) engage in irrigation and manuring agriculture. In the highland valleys that are too high for farming, shepherds look after their flocks. The most accessible town in Ladakh, Leh, is situated 160 miles (260 km) east of Srinagar and is a major trading hub.</p>
+<br>
+<div class=container>
+<img src=assets/img/backs/bikes.webp width=100% height="100%"/>
+</div>
+<br>
+<p class=justify>Ladakh has a dry, chilly climate. Roughly 3 inches (80 mm) of precipitation falls on average each year; fine, dry, flaking snow is common and can occasionally fall in large amounts. The only places where vegetation grows are valleys and protected areas. Here, tamarisk (genus Tamarix) shrubs, furze (also known as gorse; spiky plants of the legume family), and other plants grow slowly and provide much-needed firewood. Wheat, barley, millet, buckwheat, peas, beans, and turnips are the main produce. The main products are textiles, namely woolen cloth.</p>
+<p class=justify>Since the breakup of British India in 1947, India and Pakistan have been at odds over Ladakh; with the 1949 cease-fire agreement, the southeast of the region was given to India and the remaining half to Pakistan. When Chinese forces arrived in the area in the early 1960s, they took control of the part of Ladakh that was theirs.</p>
+<blockquote><i class="fas fa-quote-right"></i> <span class=quote>I actually found the place by luck, I didn’t ask anyone for directions, I just drove my car up and down the Ladakh subcontinent <span class=author> Mr. Ransom</span></span></blockquote>
+</div>
+</div>
+<div class="container mt-20 paragraph">
+<div class="content center">
+<p class=mt-50><button href class="primary-btn white b-gold border t-gold" onclick=like()><i class="fas fa-heart"></i>
+Likes: <span id=likeCount>0</span>
+</button> <button href class="primary-btn white b-gold border t-gold"><i class="fas fa-share"></i> Share</button></p>
+</div>
+</div>
+</div>
+<div class="section back five mt-50">
+<div class="container center">
+<h5 class=t-black>stay connected</h5>
+<span class="line gold"></span>
+<div class="social-container m-20 center">
+<a href class="primary-btn white icon t-gold"><i class="fab fa-facebook-square"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-instagram"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+<?php include('footer.php') ?>
+<script src=js/jquery-3.4.1.min.js></script>
+<script src=js/bootstrap.min.js></script>
+<script src=js/popper.min.js></script>
+<script src=js/slick.min.js></script>
+<script src=js/validator.min.js></script>
+<script src=js/contact.js></script>
+<script src=js/main.js></script>
+<script src=js/aos.js></script>
+<script>let likeCounter=localStorage.getItem("likeCounter")?parseInt(localStorage.getItem("likeCounter")):0;document.addEventListener("DOMContentLoaded",function(){document.getElementById("likeCount").innerText=likeCounter});function like(){likeCounter++;localStorage.setItem("likeCounter",likeCounter);document.getElementById("likeCount").innerText=likeCounter};</script>
+</body>
 </html>
