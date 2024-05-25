5f215c42d1052e95a9aacb1d9755a3ff5f94ecd8
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-05-26 00:18:20 +0530

    commit updated

---
+++ jaipur-india.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html xmlns=http://www.w3.org/1999/xhtml lang=en-US>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<title>Jaipur | History, Map, Population, & Facts | TravelGuru</title>
+<meta name=description content="Jaipur, city, capital of Rajasthan state, northwestern India. It is situated in the east-central part of the state, roughly equidistant from Alwar (northeast) and Ajmer (southwest). It is Rajasthan’s most-populous city. Learn more about Jaipur in this article.">
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<meta name=keywords content="Jaipur | History, Map, Population, & Facts | TravelGuru">
+<meta name=Resource-type content=Document>
+<link rel=icon type=image/x-icon href=assets/img/favicon.ico>
+<link href="../../css?family=Montserrat:100,200,300,400,600,700&display=swap" rel=stylesheet>
+<link href="../../css-1?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel=stylesheet>
+<link rel=stylesheet type=text/css href=css/bootstrap.min.css>
+<link rel=stylesheet type=text/css href=css/style.css>
+<link rel=stylesheet type=text/css href=css/az-styles.css>
+<script src=../../747940bb1c.js></script>
+<link rel=stylesheet type=text/css href=css/animate.css>
+<link rel=stylesheet type=text/css href=css/aos.css>
+<link rel=stylesheet type=text/css href=css/slick.css>
+<link rel=stylesheet type=text/css href=css/slick-theme.css>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9V32PPHD66"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9V32PPHD66');
+</script>
+</head>
+<body class=white>
+<?php include('header.php') ?>
+<div id=about class="section video">
+<div class=container>
+<div class="content center mt-100">
+<h5 class=t-gold>JAIPUR</h5>
+<span class=line></span>
+<div class=tags><a class="tag t-white b-gold">#traveling</a> <a class="tag t-white b-gold">#India</a> <a class="tag t-white b-gold">#vacation</a></div>
+<div class=mt-50></div>
+</div>
+</div>
+<div class=video-back>
+<div class="color-overlay op-6"></div>
+<video class="back img jaipur">
+</video>
+</div>
+</div>
+<div class="section white no-padding post-detail">
+<div class=container id=post>
+<div class="content center paragraph">
+<div class="center info-box-detail mt-minus-200">
+<div class=author><img src=assets/img/user1.jpg alt=exodus class=img-fluid></div>
+<div class=post-info>By: <a class="author t-gold">Vikram</a> from <span class=country>India</span></div>
+</div>
+<h1 class=t-black>Jaipur | History, Map, Population, & Facts</h1>
+<div class="info-box-detail left">
+<div class=post-date>6:25 pm Saturday, 25 May 2024</div>
+<span class=separation>/</span>
+<div class=post-views>
+<img src=assets/img/user1.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user2.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user3.jpg alt=exodus class=img-fluid>
+<span class=count>+34</span>
+</div>
+</div>
+<p class=justify>The capital of the northwest Indian state of Rajasthan is Jaipur. It is located in the state's east-central region, about halfway between Ajmer, which is to the southwest, and Alwar, which is to the northeast. It is the most populated city in Rajasthan.</p>
+<p class=justify>Built in 1727 by Maharaja Sawai Jai Singh, the walled town with a hilly backdrop to the south was intended to replace the adjacent Amber (now Amer, a suburb of Jaipur) as the capital of the princely kingdom of Jaipur, which had been established in the 12th century CE by the Rajputs. In the late 20th and early 21st centuries, Jaipur experienced tremendous expansion, with its population tripling between 1991 and 2011. The population is a mix of Muslims and Hindus. Early in the twenty-first century, the city was the subject of multiple bombing attacks, with mosques and Hindu temples falling victim.
+</p>
+<p class=justify>Jaipur is a well-liked travel destination with excellent air, train, and road links as well as a commercial trade hub. Manufacturing of glass, hosiery, carpets, blankets, shoes, engineering and metalworking, handloom weaving, and distilling are among the industries. Jaipur is well-known for its stone, marble, and ivory carvings in addition to its jewelry, enamel, metalwork, and printed textiles.</p>
+<br>
+<div class=container>
+<img src=assets/img/backs/jaipur2.webp width=100% height="100%"/>
+</div>
+<br>
+<p class=justify>The city is renowned for its aesthetic appeal and for having distinctive straight-line planning. Because most of its buildings are rose-colored, it is occasionally referred to as the "pink city." The main structures are the City Palace, which houses the Jaipur royal family; Jantar Mantar, an outdoor observatory from the 18th century that was inducted into the UNESCO World Heritage list in 2010; Hawa Mahal, well known as the Hall of Winds; Ram Bagh Palace; and Nahargarh, often known as the Tiger Fort. There are also libraries and museums among the public structures. The University of Rajasthan was established in 1947 and is headquartered in Jaipur.</p>
+
+<p class=justify>Fertile alluvial plains to the east and south, and hill ranges and desert regions to the north and west, encircle the city. The main crops farmed in the area are cotton, pulses, barley, gram (chickpeas), and bajra (pearl millet). Deposits of garnet, marble, copper, mica, feldspar, iron ore, and beryllium are worked. Sariska National Park to the northeast is one of the neighboring attractions in addition to Amer. Population: 2,322,575 in 2001; 3,046,163 in 2011.</p>
+<blockquote><i class="fas fa-quote-right"></i> <span class=quote>I actually found the place by luck, I didn’t ask anyone for directions, I just drove my car up and down the Jaipur <span class=author> Mr. Ransom</span></span></blockquote>
+</div>
+</div>
+<div class="container mt-20 paragraph">
+<div class="content center">
+<p class=mt-50><button href class="primary-btn white b-gold border t-gold" onclick=like()><i class="fas fa-heart"></i>
+Likes: <span id=likeCount>0</span>
+</button> <button href class="primary-btn white b-gold border t-gold"><i class="fas fa-share"></i> Share</button></p>
+</div>
+</div>
+</div>
+<div class="section back five mt-50">
+<div class="container center">
+<h5 class=t-black>stay connected</h5>
+<span class="line gold"></span>
+<div class="social-container m-20 center">
+<a href class="primary-btn white icon t-gold"><i class="fab fa-facebook-square"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-instagram"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+<?php include('footer.php') ?>
+<script src=js/jquery-3.4.1.min.js></script>
+<script src=js/bootstrap.min.js></script>
+<script src=js/popper.min.js></script>
+<script src=js/slick.min.js></script>
+<script src=js/validator.min.js></script>
+<script src=js/contact.js></script>
+<script src=js/main.js></script>
+<script src=js/aos.js></script>
+<script>let likeCounter=localStorage.getItem("likeCounter")?parseInt(localStorage.getItem("likeCounter")):0;document.addEventListener("DOMContentLoaded",function(){document.getElementById("likeCount").innerText=likeCounter});function like(){likeCounter++;localStorage.setItem("likeCounter",likeCounter);document.getElementById("likeCount").innerText=likeCounter};</script>
+</body>
+</html>
