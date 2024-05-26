90864adee7d86a9dbcdbf0afa8faf7ac9db7c079
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-05-26 13:07:36 +0530

    commit updated

---
+++ kashmir.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html xmlns=http://www.w3.org/1999/xhtml lang=en-US>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<title>Kashmir
+region, Indian subcontinent, Asia</title>
+<meta name=description content="Kashmir is a region of the northwestern Indian subcontinent. Claimed by both India and Pakistan, the region has remained a source of tension since the partition of 1947 and was the focus of two wars between them. The region is administratively partitioned along a de facto cease-fire line known as the line of control.">
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<meta name=keywords content="Kashmir
+region, Indian subcontinent, Asia">
+<meta name=Resource-type content=Document>
+<link rel=icon type=image/x-icon href=assets/img/favicon.ico>
+<link href="../../css?family=Montserrat:100,200,300,400,600,700&display=swap" rel=stylesheet>
+<link href="../../css-1?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel=stylesheet>
+<link rel=stylesheet type=text/css href=css/bootstrap.min.css>
+<link rel=stylesheet type=text/css href=css/style.css>
+<link rel=stylesheet type=text/css href=css/az-styles.css>
+<script src=../../747940bb1c.js></script>
+<link rel=stylesheet type=text/css href=css/animate.css>
+<link rel=stylesheet type=text/css href=css/aos.css>
+<link rel=stylesheet type=text/css href=css/slick.css>
+<link rel=stylesheet type=text/css href=css/slick-theme.css>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9V32PPHD66"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
+gtag('js',new Date());gtag('config','G-9V32PPHD66');</script>
+</head>
+<body class=white>
+<?php include('header.php') ?>
+<div id=about class="section video">
+<div class=container>
+<div class="content center mt-100">
+<h5 class=t-gold>KASHMIR</h5>
+<span class=line></span>
+<div class=tags><a class="tag t-white b-gold">#traveling</a> <a class="tag t-white b-gold">#India</a> <a class="tag t-white b-gold">#vacation</a></div>
+<div class=mt-50></div>
+</div>
+</div>
+<div class=video-back>
+<div class="color-overlay op-6"></div>
+<video class="back img kashmir">
+</video>
+</div>
+</div>
+<div class="section white no-padding post-detail">
+<div class=container id=post>
+<div class="content center paragraph">
+<div class="center info-box-detail mt-minus-200">
+<div class=author><img src=assets/img/user1.jpg alt=exodus class=img-fluid></div>
+<div class=post-info>By: <a class="author t-gold">Vikram</a> from <span class=country>India</span></div>
+</div>
+<h1 class=t-black>Kashmir
+region, Indian subcontinent, Asia</h1>
+<div class="info-box-detail left">
+<div class=post-date>12:22 pm Sunday, 26 May 2024</div>
+<span class=separation>/</span>
+<div class=post-views>
+<img src=assets/img/user1.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user2.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user3.jpg alt=exodus class=img-fluid>
+<span class=count>+34</span>
+</div>
+</div>
+<p class=justify>Kashmir is a part of the Indian subcontinent's northwest. It is bordered to the northeast by the Uygur Autonomous Region of Xinjiang, to the east by the Tibet Autonomous Region (both in China), to the south by the Indian states of Punjab and Himachal Pradesh, to the west by Pakistan, and to the northwest by Afghanistan. With a combined area of over 85,800 square miles (222,200 square kilometers), the territory has been a point of contention between Pakistan and India ever since the Indian subcontinent was divided in 1947. </p>
+<p class=justify>Three areas make up the northern and western parts, which are under the administration of Pakistan: Gilgit, Baltistan, and Azad Kashmir. The latter two are combined to form Gilgit-Baltistan, formerly known as the Northern Areas. The parts in the south and southeast, Jammu and Kashmir and Ladakh, are governed by India. Although neither government acknowledges it as an international boundary, a "line of control" agreed upon in 1972 divides the areas controlled by Pakistan and India. Furthermore, China started to intervene in the eastern section of Kashmir in the 1950s, and since 1962, it has ruled the northeastern province of Ladakh, which is the easternmost part of the territory.</p>
+<h4 class="left mt-40">Land and people</h4>
+<p class=justify>The Kashmir region is mostly made up of mountains, with high, desolate plateaus and deep, narrow valleys. The densely forested Himalayan foothills and the Pir Panjal Range of the Lesser Himalayas divide the comparatively low-lying Jammu and Punch (Poonch) plains in the southwest from the bigger, more fertile, and more densely populated Vale of Kashmir to the north. Approximately 5,300 feet (1,600 meters) above sea level, the vale forms the upper Jhelum River basin and is home to the city of Srinagar. The Punch lowlands are mostly in Azad Kashmir, but Jammu and Kashmir are ruled by India.</p>
+<br>
+<div class=container>
+<img src=assets/img/backs/kasmir2.webp width=100% height="100%"/>
+</div>
+<br>
+<p class=justify>The western portion of the Great Himalayas rises northeast of the valley, with peaks reaching as high as 20,000 feet (6,100 meters). The steep valley of the Indus River, which flows northwest, bisects the high, hilly plateau region of Ladakh, located further to the northeast. The towering peaks of the Karakoram Range, which stretch roughly northwestward from the Himalayas, include K2 (Mount Godwin Austen), which is the second-highest mountain in the world after Mount Everest at 28,251 feet (8,611 meters).</p>
+<p class=justify>The area is situated near the Indian-Australian tectonic plate's northernmost extremity. Heavy seismic activity has been recorded in Kashmir as a result of that plate subducting beneath the Eurasian Plate, a process that has been constructing the Himalayas for about 50 million years. Muzaffarabad, the administrative hub of Azad Kashmir, and surrounding regions, such as sections of Pakistan's North-West Frontier Province (now Khyber Pakhtunkhwa) and India's Jammu and Kashmir state (now Jammu and Kashmir union territory), were completely destroyed by an exceptionally strong earthquake in 2005.</p>
+<p class=justify>The region's climate varies from subtropical in the lowlands in the southwest to alpine in the high mountain regions. The amount of precipitation varies; it is more abundant in regions accessible to monsoonal winds, such as those west and south of the major ranges, and less in locations where continental conditions are predominant, such as the north and east.</p>
+<p class=justify>The people that live in the Jammu region speak Dogri, Punjabi, and Hindi and are Muslim in the east and Hindu in the west. The majority of people that live in the Pakistani and Vale of Kashmir regions are Muslims who speak Kashmiri and Urdu. Tibetans who speak Balti and Ladakhi and profess Buddhism live in the sparsely populated Ladakh area and beyond.</p>
+<h2 class="left mt-40">History</h2>
+<h4 class=left>The region to 1947</h4>
+<p class=justify>Legend has it that the area that is now Kashmir was taken back from a large lake by an ascetic by the name of Kashyapa. Later, that region was referred to as Kashmir and Kashyapamar. The Mauryan monarch Ashoka brought Buddhism to the region in the third century BCE, and it seems that the area gained significant significance as a center of Hindu culture between the ninth and the twelfth centuries CE. Kashmir was controlled by a string of Hindu rulers until 1346, when it was taken over by Muslims. After Kashmir was captured by the Sikh kingdom of Punjab in 1819 and later by the Dogra kingdom of Jammu in 1846, the Muslim era came to an end. It lasted about five centuries.</p>
+<p class=justify>Therefore, the Kashmir region as it exists today began in 1846 when Raja Gulab Singh, the Dogra ruler of Jammu, was made maharaja (ruling prince) of an expansive but ill-defined Himalayan kingdom “to the eastward of the River Indus and westward of the River Ravi” by the treaties of Lahore and Amritsar at the end of the First Sikh War. In the later half of the 19th century, the British were able to protect their northern flank during their push to the Indus and beyond thanks to the establishment of this princely state.
+</p>
+<p class=justify>As a result, the state was a part of the intricate political buffer that the British placed between their Indian empire and the empires of China and Russia to the north. Confirmation of title to these mountainous areas, for Gulab Singh, was the result of nearly twenty-five years of diplomatic negotiations and campaigning among the small hill kingdoms around the northern frontier of the Sikh empire of Punjab.</p>
+<p class=justify>There were some attempts in the 19th century to establish the limits of the region, but the terrain and the presence of large areas devoid of permanent human habitation frequently made accurate designation impossible. For instance, the maharaja's power in the far north was undoubtedly up to the Karakoram Range, but there was always a gray area on the frontiers between Turkistan and Xinjiang, two Central Asian provinces. </p>
+<p class=justify>Similar questions were raised over the boundary's alignment as this northern zone skirted the Aksai Chin region to the east and connected to the more accurately defined and well-known border with Tibet, which had been the eastern boundary of the Ladakh region for millennia. In the final ten years of the 1800s, Britain defined limits in the Pamirs region through agreements with Russia and Afghanistan, which helped to clarify the pattern of boundaries in the northwest. Gilgit, which was then always considered to be a part of Kashmir, was established as a separate agency in 1889 under the leadership of a British agent for geopolitical considerations.</p>
+<h2 class="left mt-40">The issue of Kashmir</h2>
+<p class=justify>The vulnerabilities in the territory's structure and along its borders were not very significant as long as the United Kingdom was guaranteeing its existence, but they became noticeable following the British withdrawal from South Asia in 1947. The rulers of princely states were granted the option to choose between Pakistan and India or, with some restrictions, to maintain their independence under the terms agreed upon by India and Pakistan for the partition of the Indian subcontinent. The maharaja of Kashmir, Hari Singh, first thought he could keep Kashmir independent by postponing his decision. </p>
+<p class=justify>However, he signed an Instrument of Accession to the Indian Union in October 1947 after being entangled in a series of events that included a revolution among his Muslim people along the state's western boundaries and the intervention of Pashtun tribesmen. This was the cue for action from India, which wanted to ratify the act of accession, as well as Pakistan, which saw the state as a logical extension of its own territory.
+Localized fighting persisted throughout 1948 and came to an end in January 1949 when a cease-fire was implemented thanks to the UN's intervention. India and Pakistan established the line of control, or cease-fire, in July of that year, dividing the territory's governance. That division, which was viewed as a temporary measure at the time, is still in place.</p>
+<h2 class="left mt-40">Attempts at justification and settlement</h2>
+<p class=justify>The region was divided as a result of political developments during and after the 1947 partition, despite the fact that Kashmir had a definite Muslim majority prior to the split and was geographically, economically, and culturally adjacent to the Muslim-majority region of Punjab. Pakistan was left with a largely Muslim yet sparsely inhabited, largely uninhabitable, and economically underdeveloped area. With its traditional outputs via the Jhelum valley route shut, the main Muslim community, estimated to number over half of the region's inhabitants, was located in Indian-administered territory in the Vale of Kashmir.</p>
+<p class=justify>Following the Chinese incursion into Ladakh in 1962, numerous suggestions were made to resolve the issue over Kashmir; nonetheless, tensions between the two nations increased, leading to the outbreak of hostilities between India and Pakistan in 1965. September saw the establishment of a cease-fire, and in early January 1966, the two sides signed an agreement in Tashkent, Uzbekistan, pledging to try to resolve the conflict peacefully. In 1971, as part of the India-Pakistan conflict that led to the establishment of Bangladesh, hostilities between the two resumed. </p>
+<p class=justify>A 1972 agreement made in the Indian city of Shimla expressed the hope that the nations in the region will eventually be able to coexist peacefully. Although he subsequently refuted this, it was widely assumed that Pakistan's prime leader at the time, Zulfikar Ali Bhutto, may have implicitly acknowledged the line of control as the de facto border. Following Bhutto's 1977 incarceration and 1979 execution, India and Pakistan's main source of conflict once more revolved around the Kashmir dispute.</p>
+<p class=justify>Different movements have advocated for Kashmir to become a part of Pakistan or for the area to become independent of both Pakistan and India. The Indian union government has kept a significant military presence along the cease-fire line, particularly since the end of the 1980s, to counter these movements and engage Pakistani forces. Up until2019, the military presence was also meant to bolster Jammu and Kashmir state's administrative integrity against efforts to grant Buddhist Ladakh Indian union territory status.</p>
+<h2 class="left mt-40">Chinese objectives</h2>
+<p class=justify>The boundary accords signed by the British in northeastern Kashmir had never been ratified by China. This situation persisted after China was overthrown by communists in 1949, despite requests from the new administration to initiate border talks with India being met with resistance. Chinese soldiers advanced into the northern regions of Ladakh following the establishment of Chinese rule in Tibet and its subsequent reassertion in Xinjiang. This was primarily done to provide the Chinese control over passes in the region separating India and Tibet and to enable the construction of a military road through the Aksai Chin plateau area, which was finished in 1956–1957. This improved connection between Xinjiang and western Tibet.</p>
+<p class=justify>The Sino-Indian War of October 1962 resulted from border skirmishes between the two nations after India's delayed discovery of this road. Since the fighting, China has taken control of the northern region of Ladakh. The episode greatly exacerbated the diplomatic rift between India and China, which did not start to mend until the late 1980s, since India refused to engage in negotiations with China on the determination of the Ladakhi boundary in this region. China attempted to strengthen ties with India in the ensuing decades, but the disputed Ladakh border remained unresolved.</p>
+<blockquote><i class="fas fa-quote-right"></i> <span class=quote>I actually found the place by luck, I didn’t ask anyone for directions, I just drove my car up and down the kashmir subcontinent of Asia <span class=author> Mr. Ransom</span></span></blockquote>
+</div>
+</div>
+<div class="container mt-20 paragraph">
+<div class="content center">
+<p class=mt-50><button href class="primary-btn white b-gold border t-gold" onclick=like()><i class="fas fa-heart"></i>
+Likes: <span id=likeCount>0</span>
+</button> <button href class="primary-btn white b-gold border t-gold" onclick="shareURL('https://www.travelguru.world/kashmir')"><i class="fas fa-share"></i> Share</button></p>
+</div>
+</div>
+</div>
+<div class="section back five mt-50">
+<div class="container center">
+<h5 class=t-black>stay connected</h5>
+<span class="line gold"></span>
+<div class="social-container m-20 center">
+<a href class="primary-btn white icon t-gold"><i class="fab fa-facebook-square"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-instagram"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+<?php include('footer.php') ?>
+<script src=js/jquery-3.4.1.min.js></script>
+<script src=js/bootstrap.min.js></script>
+<script src=js/popper.min.js></script>
+<script src=js/slick.min.js></script>
+<script src=js/validator.min.js></script>
+<script src=js/contact.js></script>
+<script src=js/main.js></script>
+<script src=js/aos.js></script>
+<script>let likeCounter=localStorage.getItem("likeCounter")?parseInt(localStorage.getItem("likeCounter")):0;document.addEventListener("DOMContentLoaded",function(){document.getElementById("likeCount").innerText=likeCounter});function like(){likeCounter++;localStorage.setItem("likeCounter",likeCounter);document.getElementById("likeCount").innerText=likeCounter};</script>
+<script>function shareURL(url){if(navigator.share){navigator.share({url:url}).then(()=>console.log('URL shared successfully')).catch((error)=>console.error('Error sharing URL:',error));}else{alert('Web Share API is not supported by your browser. You can manually share the URL by copying it: '+url);}}</script>
+</body>
+</html>
