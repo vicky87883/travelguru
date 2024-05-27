54c051203ed32d6f12e1c454fcbfc61e1c03bb79
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-05-28 00:48:23 +0530

    commit updated

---
+++ agra.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html xmlns=http://www.w3.org/1999/xhtml lang=en-US>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<title>Agra | India, Taj Mahal, Fort Agra, Mughal Capital</title>
+<meta name=description content="Agra, city, western Uttar Pradesh state, northern India. It lies in the Indo-Gangetic Plain on the Yamuna (Jumna) River about 125 miles (200 km) southeast of Delhi. There was an early reference to an “Agravana” in the ancient Sanskrit epic Mahabharata, and Ptolemy is said to have called the site">
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<meta name=keywords content="Agra | India, Taj Mahal, Fort Agra, Mughal Capital">
+<meta name=Resource-type content=Document>
+<link rel=icon type=image/x-icon href=assets/img/favicon.ico>
+<link href="../../css?family=Montserrat:100,200,300,400,600,700&display=swap" rel=stylesheet>
+<link href="../../css-1?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel=stylesheet>
+<link rel=stylesheet type=text/css href=css/bootstrap.min.css>
+<link rel=stylesheet type=text/css href=css/style.css>
+<link rel=stylesheet type=text/css href=css/az-styles.css>
+<script src=../../747940bb1c.js></script>
+<link rel=stylesheet type=text/css href=css/animate.css>
+<link rel=stylesheet type=text/css href=css/aos.css>
+<link rel=canonical href=https://www.travelguru.world/agra />
+<link rel=stylesheet type=text/css href=css/slick.css>
+<link rel=stylesheet type=text/css href=css/slick-theme.css>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9V32PPHD66"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
+gtag('js',new Date());gtag('config','G-9V32PPHD66');</script>
+</head>
+<body class=white>
+<?php include('header.php') ?>
+<div id=about class="section video">
+<div class=container>
+<div class="content center mt-100">
+<h5 class=t-gold>AGRA</h5>
+<span class=line></span>
+<div class=tags><a class="tag t-white b-gold">#traveling</a> <a class="tag t-white b-gold">#India</a> <a class="tag t-white b-gold">#vacation</a></div>
+<div class=mt-50></div>
+</div>
+</div>
+<div class=video-back>
+<div class="color-overlay op-6"></div>
+<video class="back img taj">
+</video>
+</div>
+</div>
+<div class="section white no-padding post-detail">
+<div class=container id=post>
+<div class="content center paragraph">
+<div class="center info-box-detail mt-minus-200">
+<div class=author><img src=assets/img/user1.jpg alt=exodus class=img-fluid></div>
+<div class=post-info>By: <a class="author t-gold">Vikram</a> from <span class=country>India</span></div>
+</div>
+<h1 class=t-black>Agra | India, Taj Mahal, Fort Agra, Mughal Capital</h1>
+<div class="info-box-detail left">
+<div class=post-date>12:22 pm Sunday, 26 May 2024</div>
+<span class=separation>/</span>
+<div class=post-views>
+<img src=assets/img/user1.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user2.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user3.jpg alt=exodus class=img-fluid>
+<span class=count>+34</span>
+</div>
+</div>
+<p class=justify>Agra is a city in the northern Indian state of Uttar Pradesh. Situated on the Yamuna (Jumna) River in the Indo-Gangetic Plain, it is roughly 200 kilometers (125 miles) southeast of Delhi.
+</p>
+<p class=justify>The ancient Sanskrit epic Mahabharata made reference to a "Agravana" early on, and Ptolemy is supposed to have named the location "Agra." Early in the 16th century, Sultan Sikandar of the Lodī dynasty established the city as the capital of the Delhi sultanate. During several eras of the Mughal empire, Agra also functioned as the capital. The city was overrun in the late eighteenth century by the Jats, Marathas, Mughals, the Gwalior monarch, and, in 1803, the British. From 1833 until 1868, it served as the provincial capital of Agra (after known as North-Western), and it was also one of the primary hubs of the Indian Mutiny (1857–58).</p>
+<p class="justify">The 17th-century Taj Mahal, which was named a UNESCO World Heritage monument in 1983, is the main reason for visiting Agra. Often regarded as the most exquisite example of Mughal architecture in the world, the Taj Mahal is a sophisticated mausoleum. It was constructed in the middle of the 17th century by the Mughal emperor Shah Jahān for Mumtāz Maḥal, his favorite wife. The emperor Akbar erected the Agra Fort (16th century), also known as the Red Fort because to its enormous red sandstone walls. It houses the Jahangiri Mahal palace and the Pearl Mosque (Moti Masjid; 17th century), which is made of white marble. In 1983, the fort was added to the list of World Heritage sites.</p>
+<p class="justify">Near the Taj Mahal are the Jāmiʿ Masjid, often known as the Great Mosque, and the exquisite white marble tomb of Iſtimād al-Dawlah (1628). Akbar's tomb is located in Sikandra, to the northwest.
+
+Renowned for its handwoven carpets, cut stone, and leather items, Agra is a commercial and industrial hub with several road and rail intersections. One important aspect of the city's economy is tourism. Dr. B.R. Ambedkar University (previously Agra University), established in 1927, has its headquarters in this city. The state psychiatric hospital and Dayalbagh, a colony of the Radha Soami Satsang religious group (established in the city in 1861) are located in Agra's suburbs.
+</p>
+<p class="justify">
+In Agra, there are numerous religious and cultural celebrations. The celebration of Janamashtami honors Lord Krishna's birthday. Every year in February, Shilpagram, a crafts village near the Taj Mahal, hosts the 10-day Taj Mahotsav, an arts, crafts, and music festival.
+</p>
+<p class="justify">The region around Agra consists almost entirely of a level plain, with hills in the extreme southwest. The region is watered by the Yamuna River and the Agra Canal. Millet, barley, wheat, and cotton are among the crops grown. The deserted Mughal city of Fatehpur Sikri is about 25 miles (40 km) southwest of Agra city. Pop. (2001) city, 1,275,134; urban agglom., 1,331,339; (2011) city, 1,585,704; urban agglom., 1,760,285.</p>
+<blockquote><i class="fas fa-quote-right"></i> <span class=quote>I actually found the place by luck, I didn’t ask anyone for directions, I just drove my car up and down the Agra <span class=author> Mr. Ransom</span></span></blockquote>
+</div>
+</div>
+<div class="container mt-20 paragraph">
+<div class="content center">
+<p class=mt-50><button href class="primary-btn white b-gold border t-gold" onclick=like()><i class="fas fa-heart"></i>
+Likes: <span id=likeCount>0</span>
+</button> <button href class="primary-btn white b-gold border t-gold" onclick="shareURL('https://www.travelguru.world/kashmir')"><i class="fas fa-share"></i> Share</button></p>
+</div>
+</div>
+</div>
+<div class="section back five mt-50">
+<div class="container center">
+<h5 class=t-black>stay connected</h5>
+<span class="line gold"></span>
+<div class="social-container m-20 center">
+<a href class="primary-btn white icon t-gold"><i class="fab fa-facebook-square"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-instagram"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+<?php include('footer.php') ?>
+<script src=js/jquery-3.4.1.min.js></script>
+<script src=js/bootstrap.min.js></script>
+<script src=js/popper.min.js></script>
+<script src=js/slick.min.js></script>
+<script src=js/validator.min.js></script>
+<script src=js/contact.js></script>
+<script src=js/main.js></script>
+<script src=js/aos.js></script>
+<script>let likeCounter=localStorage.getItem("likeCounter")?parseInt(localStorage.getItem("likeCounter")):0;document.addEventListener("DOMContentLoaded",function(){document.getElementById("likeCount").innerText=likeCounter});function like(){likeCounter++;localStorage.setItem("likeCounter",likeCounter);document.getElementById("likeCount").innerText=likeCounter};</script>
+<script>function shareURL(url){if(navigator.share){navigator.share({url:url}).then(()=>console.log('URL shared successfully')).catch((error)=>console.error('Error sharing URL:',error));}else{alert('Web Share API is not supported by your browser. You can manually share the URL by copying it: '+url);}}</script>
+</body>
+</html>
