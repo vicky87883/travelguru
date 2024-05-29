7d8811d5a0cb1d4f2f54c0a14d386885b6fc0f4b
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-05-29 19:36:03 +0530

    commit updated

---
+++ nashik.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html xmlns=http://www.w3.org/1999/xhtml lang=en-US>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<title>Nashik | Wine Capital, Temples, Fairs</title>
+<meta name=description content="Nashik, city, northwestern Maharashtra state, western India. It lies in the Western Ghats along the Godavari River and is situated along major road and rail routes at a point about 110 miles (180 km) northeast of Mumbai (Bombay). Nashik is an important religious centre and attracts thousands of">
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<meta name=keywords content="Nashik, encyclopedia, encyclopeadia, travelguru, article">
+<meta name=Resource-type content=Document>
+<link rel=icon type=image/x-icon href=assets/img/favicon.ico>
+<link href="../../css?family=Montserrat:100,200,300,400,600,700&display=swap" rel=stylesheet>
+<link href="../../css-1?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel=stylesheet>
+<link rel=stylesheet type=text/css href=css/bootstrap.min.css>
+<link rel=stylesheet type=text/css href=css/style.css>
+<link rel=stylesheet type=text/css href=css/az-styles.css>
+<script src=../../747940bb1c.js></script>
+<link rel=stylesheet type=text/css href=css/animate.css>
+<link rel=canonical href=https://www.travelguru.world/nashik />
+<link rel=stylesheet type=text/css href=css/aos.css>
+<link rel=stylesheet type=text/css href=css/slick.css>
+<link rel=stylesheet type=text/css href=css/slick-theme.css>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9V32PPHD66"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9V32PPHD66');
+</script>
+</head>
+<body class=white>
+<?php include('header.php') ?>
+<div id=about class="section video">
+<div class=container>
+<div class="content center mt-100">
+<h5 class=t-gold>NASHIK</h5>
+<span class=line></span>
+<div class=tags><a class="tag t-white b-gold">#traveling</a> <a class="tag t-white b-gold">#India</a> <a class="tag t-white b-gold">#vacation</a></div>
+<div class=mt-50></div>
+</div>
+</div>
+<div class=video-back>
+<div class="color-overlay op-6"></div>
+<video class="back img ladakh">
+</video>
+</div>
+</div>
+<div class="section white no-padding post-detail">
+<div class=container id=post>
+<div class="content center paragraph">
+<div class="center info-box-detail mt-minus-200">
+<div class=author><img src=assets/img/user1.jpg alt=exodus class=img-fluid></div>
+<div class=post-info>By: <a class="author t-gold">Vikram</a> from <span class=country>India</span></div>
+</div>
+<h1 class=t-black>Nashik | Wine Capital, Temples, Fairs</h1>
+<div class="info-box-detail left">
+<div class=post-date>6:25 pm Saturday, 25 May 2024</div>
+<span class=separation>/</span>
+<div class=post-views>
+<img src=assets/img/user1.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user2.jpg alt=exodus class=img-fluid>
+<img src=assets/img/user3.jpg alt=exodus class=img-fluid>
+<span class=count>+34</span>
+</div>
+</div>
+<p class=justify>Nashik is a city in the northwest Indian state of Maharashtra. Situated around 110 miles (180 km) northeast of Mumbai (Bombay), it is situated along important road and rail links in the Western Ghats along the Godavari River.</p>
+<p class="justify">Because of the Godavari River's sacredness and the myth that the epic Rama resided there for a while with his wife Sita and brother Lakshmana, Nashik is a major religious hub that draws thousands of pilgrims each year. The city's central area is located on the river's right (south) bank. There are multiple temples in Panchavati, a quarter on the left bank. Ghats, or tiered bathing areas, line the city's riverbanks. The Pandu (Buddhist) and Chamar (Jain) cave temples, which date to the first century CE, are located in Nashik. The holiest Hindu temples in the area are Kala Ram and Gora Ram. The most significant of the pilgrimage sites, the Shaivite Jyotirlinga temple, is located in the village of Tryambakeshvar, which is 14 miles (22 km) from Nashik.</p>
+<p>The city had industrialized by the latter part of the 20th century. Processing sugar and oil, as well as weaving silk and cotton, are significant. Ozar is a township in the suburbs. Numerous colleges in Nashik are connected to the University of Pune. The Girna and Godavari rivers flow across wide, rich valleys, draining the region where Nashik is located. The main crops farmed in the area are peanuts (groundnuts), millet, and wheat. One significant irrigated cash crop is sugar. The area is renowned for its wine production as well. Spinning and weaving cotton, as well as processing sugar and oil, make up the majority of regional industry. There is a neighboring military aircraft manufacturing. City population (2001): 1,077,236; (2011): 1,486,053.</p>
+<blockquote><i class="fas fa-quote-right"></i> <span class=quote>I actually found the place by luck, I didn’t ask anyone for directions, I just drove my car up and down the NASHIK. <span class=author> Mr. Ransom</span></span></blockquote>
+</div>
+</div>
+<div class="container mt-20 paragraph">
+<div class="content center">
+<p class=mt-50><button href class="primary-btn white b-gold border t-gold" onclick=like()><i class="fas fa-heart"></i>
+Likes: <span id=likeCount>0</span>
+</button> <button href class="primary-btn white b-gold border t-gold" onclick="shareURL('https://www.travelguru.world/nashik')"><i class="fas fa-share"></i> Share</button></p>
+</div>
+</div>
+</div>
+<div class="section back five mt-50">
+<div class="container center">
+<h5 class=t-black>stay connected</h5>
+<span class="line gold"></span>
+<div class="social-container m-20 center">
+<a href class="primary-btn white icon t-gold"><i class="fab fa-facebook-square"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-instagram"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+<?php include('footer.php') ?>
+<script src=js/jquery-3.4.1.min.js></script>
+<script src=js/bootstrap.min.js></script>
+<script src=js/popper.min.js></script>
+<script src=js/slick.min.js></script>
+<script src=js/validator.min.js></script>
+<script src=js/contact.js></script>
+<script src=js/main.js></script>
+<script src=js/aos.js></script>
+<script>let likeCounter=localStorage.getItem("likeCounter")?parseInt(localStorage.getItem("likeCounter")):0;document.addEventListener("DOMContentLoaded",function(){document.getElementById("likeCount").innerText=likeCounter});function like(){likeCounter++;localStorage.setItem("likeCounter",likeCounter);document.getElementById("likeCount").innerText=likeCounter};</script>
+<script>
+        function shareURL(url) {
+            // Check if Web Share API is supported by the browser
+            if (navigator.share) {
+                navigator.share({
+                    url: url
+                })
+                .then(() => console.log('URL shared successfully'))
+                .catch((error) => console.error('Error sharing URL:', error));
+            } else {
+                // Fallback for browsers that do not support Web Share API
+                alert('Web Share API is not supported by your browser. You can manually share the URL by copying it: ' + url);
+            }
+        }
+    </script>
+</body>
+</html>
