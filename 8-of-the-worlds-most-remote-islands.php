b7be607e1745c3d5998a319e03b701d9ec5bcfbe
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-06-16 23:20:06 +0530

    commit updated

--- 8-of-the-worlds-most-remote-islands.php
+++ 8-of-the-worlds-most-remote-islands.php
@@ -42,7 +42,7 @@
 </div>
 <div class="section white no-padding post-detail">
 <div class=container id=post>
-<div  class="content center paragraph">
+<div class="content center paragraph">
 <div class="center info-box-detail mt-minus-200">
 <div class=author><img src=assets/img/user1.jpg alt=exodus class=img-fluid></div>
 <div class=post-info>By: <a class="author t-gold">Vikram</a> from <span class=country>India</span></div>
@@ -61,26 +61,34 @@ Sunday, 16 June 2024</div>
 </div>
 <p class=justify>There are still areas of the world that very few people visit, even in the twenty-first century. Some isolated locations that instantly spring to mind include lonely mountain peaks, desert interiors, Arctic ice floes, or the enormous frozen ice sheets of Antarctica. However, what about remote islands full of adventure? In this day and age, are there still any? Some of the world's most isolated locations are islands that are so cut off from other landmasses—or from the main air and maritime routes—that the rest of the world can occasionally easily forget about them. </p>
 <p class="justify">Just eight of these locations are included here; there are many more that could be included. Every island (or set of islands, in certain situations) is a dependent nation or an outlying territory of a bigger nation; they are all isolated, fascinating locations that never cease to pique the curiosity of explorers and adventurers.</p>
-<div class="left"><strong>The Kerguelen Islands</strong></div>
+<h3 class="t-black justify bold">The Kerguelen Islands</h3>
+<img src="assets/img/karguan.webp" alt="" width="100%" height="100%">
 <p class="justify">A collection of wind-tossed islands in the Indian Ocean, the Kerguelen Islands are home to mountains, rocky outcrops, glaciers, and expansive plains covered in mosses and tussock grasses. The Kerguelen Islands are not the ideal place for people to reside, with daily mean temperatures varying from 2.1 to 8.2 °C (35.8 to 46.8 °F). However, the islands offer a home for seals, albatrosses, terns, and four species of penguins.</p>
 <h3 class="t-black justify bold">Spitsbergen</h3>
+<img src="assets/img/spits.webp" alt="" width="100%" height="100%">
 <p class="justify">Spitsbergen is the largest island in the Svalbard archipelago and the largest island in Norway, covering an area of 39,044 square km (15,075 square miles). It is not surprising that the island is covered in snow and ice and is home to a significant population of polar bears given its position, which is roughly 950 km (about 590 miles) north of the coast of Europe and 830 km (about 516 miles) east of the coast of Greenland. The primary city of the island is Longyear, also known as Longyearbyen. It is located less than 3.2 kilometers (2 miles) from the Svalbard Global Seed Vault, a safe haven carved out of a mountainside that was created to protect the seeds of all of the world's food plants in case of an international emergency.</p>
 <h3 class="t-black justify bold">Pitcairn Island</h3>
+<img src="assets/img/pit.webp" alt="" width="100%" height="100%">
 <p class="justify">The only inhabited island in the British overseas territory of Pitcairn, Henderson, Ducie, and Oeno islands, this small volcanic island in the South Pacific is most renowned for being the haven of the British ship HMS Bounty's mutineers, who made their home there in 1790. Currently, Pitcairn Island sits at the heart of one of the largest marine reserves in the world, a huge expanse of open water covering 830,000 square kilometers (322,000 square miles), more than the state of Texas in the United States.</p>
 <h3 class="t-black justify bold">Novaya Zemlya</h3>
+<img src="assets/img/nova.webp" alt="" width="100%" height="100%">
 <p class="justify">Across Russia's northwest coast, the Russian-managed archipelago of Novaya Zemlya (or "New Land") consists of two sizable Arctic islands and a few smaller ones that divide the Barents and Kara seas. The two main islands, Yuzhny (southern) and Severny (northern), are divided by the tiny strait of Matochkin Shar, which is just 1.6 to 2.4 km (approximately 1 to 1.5 miles) wide. The islands are aligned for 600 miles (1,000 km) in a southwest-northeast direction. Between 1954 and 1990, Novaya Zemlya hosted over 100 nuclear tests during the Cold War.</p>
 <h3 class="t-black justify bold">Tristan da Cunha</strong></h3>
+<img src="assets/img/tris.webp" alt="" width="100%" height="100%">
 <p class="justify">The remote islands that make up Ascension, Tristan da Cunha, and St. Helena are British overseas territories. The closest inhabited mainland is St. Helena, located about 2,100 km (1,300 miles) south of Tristan da Cunha, the southernmost inhabited island in the region (together with a natural reserve made up of Inaccessible, Nightingale, Middle, Gough, and Stoltenhoff islands). The island of Tristan da Cunha is almost round in shape, with a 21-mile (34-kilometer) shoreline and a central volcanic cone that rises to a height of 2,060 meters (6,760 feet), often shrouded in clouds.</p>
 <h3 class="t-black justify bold">Easter Island</h3>
+<img src="assets/img/easter.webp" alt="" width="100%" height="100%">
 <p class="justify">A small volcanic island in the South Pacific is known by several names: Easter Island, Rapa Nui (also known as "Great Rapa"), and Te Pito te Henua (also known as "Navel of the World"). Easter Island is possibly the most isolated spot on Earth; it is situated 3,767 km (2,340 miles) from Santiago, Chile, the government that oversees it, and 2,088 km (1,300 miles) from Pitcairn Island. This 163-square-kilometer (63-square-mile) island is well-known for its enormous stone sculptures, known as the mysterious moai, of which there are over 600, and for the remains of enormous stone platforms, known as ahus, which have open courtyards on their landward sides and some of which exhibit exquisite craftsmanship.</p>
 <h3 class="t-black justify bold">South Georgia</h3>
+<img src="assets/img/georgia.webp" alt="" width="100%" height="100%">
 <p class="justify">Located in the Atlantic section of the Southern Ocean, roughly 1,450 km (900 mi) east of the Falkland Islands and 4,790 km (2,976 mi) west of Cape Town, South Africa, the island of South Georgia is a part of the British overseas territory of South Georgia and the South Sandwich Islands. There are not many human inhabitants on the island, despite the abundance of wildlife on the island and in the surrounding waterways.</p>
 <p class="justify">The only people living on the island are a small group of scientists and support staff who work at the British Antarctic Survey stations at Grytviken on King Edward Point and at Bird Island, off the island's northwest tip. The island is most known for being the location of the last leg of the difficult journey made by British explorer Sir Ernest Shackleton, who first crossed South Georgia Island in 1916 while looking for assistance for his tragic trans-Antarctic expedition. The island was used as a base for whaling and scientific expeditions on occasion during the 19th century.</p>
 <h3 class="t-black justify bold">Diego Garcia</h3>
+<img src="assets/img/diego.JPG" alt="" width="100%" height="100%">
 <p class="justify">Diego Garcia is an intriguing coral atoll with a v-shape located in the middle of the Indian Ocean. The island is the biggest and most southernly located component of the British Indian Ocean Territory's Chagos Archipelago. Because of its advantageous location between East Africa, the Middle East, Southeast Asia, and Australia, the island serves as a major air and naval support facility for the armed forces of the United States and Great Britain.</p>
 <p class="justify">(Although there is a lot going on on this island, one could argue that Diego Garcia's military goal, which is somewhat covert, justifies its inclusion in this list.) More than a thousand islanders had lived on Diego Garcia until they were forced to relocate to Mauritius and the Seychelles in 1971 to create room for the island to become militarized. The right to go back home is still being contested by these islanders and their descendants in court.</p>
 
-<blockquote><i class="fas fa-quote-right"></i> <span class=quote>I actually found the place by luck, I didn’t ask anyone for directions, <span class=author> Mr. Ransom</span></span></blockquote>
+<blockquote><i class="fas fa-quote-right"></i> <span class=quote>I think this article will help for you <span class=author> Mr. Vikram</span></span></blockquote>
 </div>
 </div>
 <div class="container mt-20 paragraph">
