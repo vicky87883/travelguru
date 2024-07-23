e2f4d8fc7780adac74ff64b4c715a95df1923d56
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-07-23 09:41:48 +0530

    commit updated

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ $result = mysqli_query($con,$query);
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755"
      crossorigin="anonymous"></script>
      <!-- Google tag (gtag.js) -->
+      
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-9V32PPHD66"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
