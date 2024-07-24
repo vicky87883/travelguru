07d812f1cf6f758268474b1a403a817a52b8852d
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-07-24 09:57:43 +0530

    commit updated

--- rss_feed.php
+++ rss_feed.php
@@ -12,23 +12,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Fetch data from travelblog table
+$sql = "SELECT title, image, link FROM travelblog";
+$result = $conn->query($sql);
 
-// Insert blog data into the travelblog table
-foreach ($blogs as $blog) {
-    $title = $conn->real_escape_string($blog["title"]);
-    $image = $conn->real_escape_string($blog["image"]);
-    $link = $conn->real_escape_string($blog["link"]);
-
-    $sql = "INSERT INTO travelblog (title, image, link) VALUES ('$title', '$image', '$link')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully<br>";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
-
-// Create RSS XML
+// Initialize RSS feed
 $rss_feed = "<?xml version='1.0' encoding='UTF-8' ?>";
 $rss_feed .= "<rss version='2.0'>";
 $rss_feed .= "<channel>";
@@ -36,21 +24,22 @@ $rss_feed .= "<title>TravelGuru Blog</title>";
 $rss_feed .= "<link>https://www.travelguru.world/blog-page</link>";
 $rss_feed .= "<description>Latest TravelGuru Blog Posts</description>";
 
-foreach ($blogs as $blog) {
-    $rss_feed .= "<item>";
-    $rss_feed .= "<title>" . htmlspecialchars($blog["title"]) . "</title>";
-    $rss_feed .= "<link>" . htmlspecialchars($blog["link"]) . "</link>";
-    $rss_feed .= "<description><![CDATA[<img src='" . htmlspecialchars($blog["image"]) . "' />]]></description>";
-    $rss_feed .= "</item>";
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $rss_feed .= "<item>";
+        $rss_feed .= "<title>" . htmlspecialchars($row["title"]) . "</title>";
+        $rss_feed .= "<link>" . htmlspecialchars($row["link"]) . "</link>";
+        $rss_feed .= "<description><![CDATA[<img src='" . htmlspecialchars($row["image"]) . "' />]]></description>";
+        $rss_feed .= "</item>";
+    }
 }
 
 $rss_feed .= "</channel>";
 $rss_feed .= "</rss>";
 
-// Save RSS feed to a file
-file_put_contents("rss_feed.xml", $rss_feed);
-
-echo "RSS feed created successfully.";
+// Output RSS feed
+header("Content-Type: application/rss+xml");
+echo $rss_feed;
 
 $conn->close();
 ?>
