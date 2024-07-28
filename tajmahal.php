cdb4ea02d2ae45c1396e2675bc5c1e49abe329fc
vicky87883 <142141519+vicky87883@users.noreply.github.com>
2024-07-28 21:40:47 +0530

    commit updated

---
+++ tajmahal.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html xmlns=http://www.w3.org/1999/xhtml lang=en-US>
+<head>
+<meta http-equiv=Content-Type content="text/html; charset=utf-8">
+<title>The Taj Mahal is widely considered one of the most beautiful buildings ever created</title>
+<meta name=description content="The Taj Mahal is widely considered one of the most beautiful buildings ever created.">
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+<meta name=keywords content="Kashmir
+region, Indian subcontinent, Asia">
+<meta name=Resource-type content=Document>
+<link rel=icon type=image/x-icon href=assets/img/favicon.ico>
+<link href="../../css?family=Montserrat:100,200,300,400,600,700&display=swap" rel=stylesheet>
+<link href="../../css-1?family=Playfair+Display:400,400i,700,700i,900,900i&display=swap" rel=stylesheet>
+<link rel=stylesheet type=text/css href=css/bootstrap.min.css>
+<link rel=stylesheet type=text/css href=css/style.css>
+<link rel=stylesheet type=text/css href=css/az-styles.css>
+<script src=../../747940bb1c.js></script>
+<link rel=stylesheet type=text/css href=css/animate.css>
+<link rel=stylesheet type=text/css href=css/aos.css>
+<link rel=canonical href=https://www.travelguru.world/kashmir />
+<link rel=stylesheet type=text/css href=css/slick.css>
+<link rel=stylesheet type=text/css href=css/slick-theme.css>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6958761602872755" crossorigin=anonymous></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9V32PPHD66"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}
+gtag('js',new Date());gtag('config','G-9V32PPHD66');</script>
+</head>
+<body class=white>
+<?php include('header.php') ?>
+<div id=about class="section video">
+<div class=container>
+<div class="content center mt-100">
+<h5 class=t-gold>KASHMIR</h5>
+<span class=line></span>
+<div class=tags><a class="tag t-white b-gold">#traveling</a> <a class="tag t-white b-gold">#India</a> <a class="tag t-white b-gold">#vacation</a></div>
+<div class=mt-50></div>
+</div>
+</div>
+<div class=video-back>
+<div class="color-overlay op-6"></div>
+<video class="back img tajmahal">
+</video>
+</div>
+</div>
+<div class="section white no-padding post-detail">
+<div class=container id=post>
+<div class="content center paragraph">
+<div class="center info-box-detail mt-minus-200">
+<div class=author><img src=assets/img/user1.jpg alt=exodus class=img-fluid></div>
+<div class=post-info>By: <a class="author t-gold">Vikram</a> from <span class=country>India</span></div>
+</div>
+<h1 class=t-black>An Immense Memorial to Love Is This Massive Tomb</h1>
+<div class="info-box-detail left">
+<div class=post-date>8:22 pm
+Sunday, 28 July 2024</div>
+<span class=separation>/</span>
+</div>
+<p class=justify>Many people agree that the Taj Mahal is among the most stunning structures ever built. The magnificent tomb in Agra, India, is a lasting tribute to a husband's love for his favorite wife. It is made of marble. It's also a timeless monument to the achievements of an ancient empire in the arts and sciences.
+</p>
+<p class="justify">
+When Shah Jahan, often known as "the King of the World," assumed the throne of the Mughal Empire in 1628, he was deeply infatuated with Mumtaz Mahal, the queen whom he called the "Chosen One of the Palace." Her beauty was so great, according to the poets of the Mughal court at Agra, that the moon blushed in front of her.
+</p>
+<p class="justify">
+Shah Jahan's reign saw the Mughals reach its zenith in terms of riches and power, thanks to India's abundant supply of priceless gems. However, he was unable to prevent Mumtaz Mahal's 1631 death during childbirth. According to legend, she bound him by promising on her deathbed to have the most exquisite mausoleum ever constructed.
+</p>
+<h4 class="left">World Heritage Site Pictures: Taj Mahal</h4>
+<div class="container">
+    <img src="assets/img/taj.jpg" alt="" width="100%" height="100%">
+</div>
+<br>
+<br>
+<p class="justify">
+Promise or not, Shah Jahan dedicated all of his resources and enthusiasm to building just this kind of monument. Under the supervision of a group of architects, it is estimated that 20,000 stone carvers, masons, and artisans from all over India, as well as Turkey and Iraq, were hired to construct the Taj Mahal in the verdant gardens by the Jamuna River in Agra. The arduous task was finished between 1631 and 1648.
+</p>
+<p class="justify">While the Taj Mahal's iconic arch-and-dome profile of pure white marble has earned recognition, its amazing details are what really make it beautiful: semiprecious stones and carvings set into the interior, along with calligraphed Koranic verses, create an appealing space where Shah Jahan visited his wife's remains before being buried next to her.
+</p>
+<p class="justify">Four minarets, from which Muslims are summoned to prayer, frame the famous marble domes of the Taj Mahal. The purpose of their small outward lean is probably to safeguard the main tomb in the event that one of them collapses.
+
+To either side of the main tomb are two other red sandstone buildings. One is a mosque, located to the west. One used to be a guesthouse.
+
+These structures are surrounded by lush gardens, and one of the gardens has a massive reflecting pool that is designed to do something that no human has ever been able to do: replicate the Taj Mahal's breathtaking magnificence.
+</p>
+<p>Even as a prisoner, not a monarch, Shah Jahan himself stared at that exquisite sight until the end of his life. After seizing the Mughal throne, his son Aurangzeb locked his father away in Agra's Red Fort, a well-liked tourist destination and World Heritage site. Shah Jahan ordered a view of the Taj Mahal from his window, either as a comfort or a punishment.
+</p>
+<h4 class="left">How to Get There</h4>
+<p class="justify">In the Indian state of Uttar Pradesh, Agra is a big city that welcomes a lot of tourists—roughly three million people visit the Taj Mahal annually. There are numerous tourist attractions in the city, which is reachable by bus, train, and (limited) air travel. The Taj Mahal complex can be reached by foot.
+</p>
+<h4 class="left">When to Visit</h4>
+<p class="justify">The Taj Mahal is a popular attraction all year round, though new ticketing procedures have reduced the number of visitors during prime hours. If you're looking for more privacy, consider arriving early or late in the day. The Taj Mahal is closed on Fridays, so that is one day you should NOT visit.
+</p>
+<h4 class="left">How to Visit</h4>
+<p class="justify">The interior of the tomb is a stunning, if somewhat cramped, area that invites careful inspection. However, a significant portion of any trip to the Taj Mahal will be devoted to taking in the structure from the outside. It's worth staying to look at the mausoleum in various lighting situations, such as the rosy glow of sunrise or the ethereal light of a full moon, since its pure white marble changes color and tone to reflect the mood of the surroundings.
+</p>
+</div>
+</div>
+<div class="container mt-20 paragraph">
+<div class="content center">
+<p class=mt-50><button href class="primary-btn white b-gold border t-gold" onclick=like()><i class="fas fa-heart"></i>
+Likes: <span id=likeCount>0</span>
+</button> <button href class="primary-btn white b-gold border t-gold" onclick="shareURL('https://www.travelguru.world/kashmir')"><i class="fas fa-share"></i> Share</button></p>
+</div>
+</div>
+</div>
+<div class="section back five mt-50">
+<div class="container center">
+<h5 class=t-black>stay connected</h5>
+<span class="line gold"></span>
+<div class="social-container m-20 center">
+<a href class="primary-btn white icon t-gold"><i class="fab fa-facebook-square"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-instagram"></i></a>
+<a href class="primary-btn white icon t-gold"><i class="fab fa-linkedin"></i></a>
+</div>
+</div>
+</div>
+<?php include('footer.php') ?>
+<script src=js/jquery-3.4.1.min.js></script>
+<script src=js/bootstrap.min.js></script>
+<script src=js/popper.min.js></script>
+<script src=js/slick.min.js></script>
+<script src=js/validator.min.js></script>
+<script src=js/contact.js></script>
+<script src=js/main.js></script>
+<script src=js/aos.js></script>
+<script>let likeCounter=localStorage.getItem("likeCounter")?parseInt(localStorage.getItem("likeCounter")):0;document.addEventListener("DOMContentLoaded",function(){document.getElementById("likeCount").innerText=likeCounter});function like(){likeCounter++;localStorage.setItem("likeCounter",likeCounter);document.getElementById("likeCount").innerText=likeCounter};</script>
+<script>function shareURL(url){if(navigator.share){navigator.share({url:url}).then(()=>console.log('URL shared successfully')).catch((error)=>console.error('Error sharing URL:',error));}else{alert('Web Share API is not supported by your browser. You can manually share the URL by copying it: '+url);}}</script>
+</body>
+</html>
